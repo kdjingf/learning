afee78cd477404a02502b3fb7db4f7e299b2607e
kdj <111>
2021-11-24 17:00:01 +0800

    1

---
+++ src/MD.java
@@ -0,0 +1,16 @@
+/**
+ * @Author: KDJ
+ * @Date: 2021/8/21 15:45
+ */
+public class MD {
+
+    public static String isPalindrome = "是否是回文数";
+    public static String romanToInt = "罗马数转整形";
+
+
+    public interface S {
+        public static String ChainOfResponsibilityPattern = "职责链模式";
+        public static String SingletonPattern = "单例模式";
+        public static String SimpleFactory = "简单工厂模式";
+    }
+}
